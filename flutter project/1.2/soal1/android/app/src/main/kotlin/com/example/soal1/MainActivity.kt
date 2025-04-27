008e85aed28e8371ac2ca7734faf948dc4e0e05d
divaasanjaya <147806522+divaasanjaya@users.noreply.github.com>
2025-04-27 23:33:43 +0700

    Delete flutter project directory

--- flutter project/1.2/soal1/android/app/src/main/kotlin/com/example/soal1/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.soal1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- flutter project/1.2/soal2/bin/soal2.dart
+++
@@ -1,5 +0,0 @@
-import 'package:soal2/soal2.dart' as soal2;
-
-void main(List<String> arguments) {
-  print('Hello world: ${soal2.calculate()}!');
-}
